0ff7694c0cb5e72fc703d912993bf5006f12ef4d
Alain Belair <belair.alain@gmail.com>
2017-12-12 11:50:23 -0500

    test style ci comment

--- src/components/Component.php
+++ src/components/Component.php
@@ -1,4 +1,15 @@
 <?php
+/* =====================================================================
+ * atk-wordpress => Wordpress interface for Agile Toolkit Framework.
+ *
+ * This interface enable the use of the Agile Toolkit framework within a WordPress site.
+ *
+ * Please note that when atk is mentioned it generally refer to Agile Toolkit.
+ * More information on Agile Toolkit: http://www.agiletoolkit.org
+ *
+ * Author: Alain Belair
+ * Licensed under MIT
+ * =====================================================================*/
 /**
  * Atk base component for Wordpress.
  * Component is an atk view that can be displayed inside WP.
@@ -13,5 +24,10 @@ use atkwp\AtkWpView;
 
 class Component extends AtkWpView
 {
+    /**
+     * The default template file name for this component.
+     *
+     * @var string 
+     */
     public $defaultTemplate = 'component.html';
 }
